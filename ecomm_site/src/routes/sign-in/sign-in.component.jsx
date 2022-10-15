08e2af8840ac543a01358ae037f6cb511b098a42
EvanHyett <evan1hyett@gmail.com>
2022-10-14 19:49:31 -0700

    updates to log in form

--- ecomm_site/src/routes/sign-in/sign-in.component.jsx
+++ ecomm_site/src/routes/sign-in/sign-in.component.jsx
@@ -1,16 +1,26 @@
-import { signInWithGooglePopup } from '../../utils/firebase/firebase.utils';
-import { createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils';
+import { useEffect } from 'react';
+import { getRedirectResult } from 'firebase/auth';
+import { auth, signInWithGooglePopup, signInWithGoogleRedirect, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils';
+
+import SignUpForm from '../../components/sign-up-form/sign-up-form.component';
 
 const SignIn = () => {
+    // useEffect(async () => {
+    //     const response = await getRedirectResult(auth);
+    //     console.log(response);
+    // }, []);
+
     const logGoogleUser = async () => {
         const {user} = await signInWithGooglePopup();
         const userDocRef = await createUserDocumentFromAuth(user);
-    }
+    };
 
     return(
         <div>
             <h1>Sign In Page</h1>
-            <button onClick={logGoogleUser}>Sign in With Google</button>
+            <button onClick={logGoogleUser}>Sign in With Google Popup</button>
+            <SignUpForm />
+            {/* <button onClick={signInWithGoogleRedirect}>Sign in With Google Redirect</button> */}
         </div>
     )
 }
