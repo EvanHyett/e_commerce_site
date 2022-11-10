d1724f731645b7b56b77e5a0f0dcf10b3974784a
EvanHyett <evan1hyett@gmail.com>
2022-11-10 11:24:20 -0800

    created checkout page

--- ecomm_site/src/contexts/cart.context.jsx
+++ ecomm_site/src/contexts/cart.context.jsx
@@ -13,12 +13,29 @@ const addCartItem = (cartItems, productToAdd) => {
     return [...cartItems, {...productToAdd, quantity: 1}];
 };
 
+const removeCartItem = (cartItems, cartItemToRemove) => {
+    const existingCartItem = cartItems.find(
+        (cartItem) => cartItem.id === cartItemToRemove.id
+    );
+
+    if(existingCartItem.quantity ===  1) {
+        return cartItems.filter(cartItem => cartItem.id != cartItemToRemove.id);
+    }
+
+    return cartItems.map((cartItem) => 
+        cartItem.id === cartItemToRemove.id
+            ? {...cartItem, quantity: cartItem.quantity - 1}
+            : cartItem
+    );
+};
+
 export const CartContext = createContext({
     isCartOpen: false,
     setIsCartOpen: () => {},
     cartItems: [],
     addItemToCart: () => {},
-    cartCount: 0
+    removeItemFromCart: () => {},
+    cartCount: 0,
 });
 
 export const CartProvider = ({children}) => {
@@ -34,8 +51,12 @@ export const CartProvider = ({children}) => {
     const addItemToCart = (productToAdd) => {
         setCartItems(addCartItem(cartItems, productToAdd))
     }
+    
+    const removeItemToCart = (cartItemToRemove) => {
+        setCartItems(removeCartItem(cartItems, cartItemToRemove))
+    }
 
-    const value = {isCartOpen, setIsCartOpen, addItemToCart, cartItems, cartCount};
+    const value = {isCartOpen, setIsCartOpen, addItemToCart, removeItemToCart, cartItems, cartCount};
 
     return <CartContext.Provider value={value}>{children}</CartContext.Provider>
 }
